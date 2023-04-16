4ad0ae538014f7946bccfc67ba7af9d727290a91
Yash Chauhan <yashc22@iitk.ac.in>
2023-04-16 12:16:41 +0530

    AddingFile

---
+++ task.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Car {
+    String make;
+    String model;
+    int year, x, y, speed;
+
+    void accelerate(speed_increment)
+    {
+        speed+=speed_increment;
+    }
+    
+}
